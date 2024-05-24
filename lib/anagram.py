43c3ce4dc8d68d5422f2714636b5e6a9ce34a008
Robert Gathanua <robertgathanua@gamail.com>
2024-05-24 06:21:59 +0300

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
-# your code goes here!
+# your code here!
+class Anagram:
+    def __init__(self, word):
+        self.word_letters = sorted([letter for letter in word])
+ 
+    def match(self, words):
+        match_words = []
+        for word in words:
+            if sorted([letter for letter in word]) == self.word_letters:
+                match_words.append(word)
+
+        return match_words        
